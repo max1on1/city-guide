c4322b374c4735eb3657482358486d84f2a9b84c
max1on1 <2630161@gmail.com>
2016-08-11 10:27:31 +0300

    add Comments controller for excursions

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
 
   resources :cities do
-    resources :excursions
+    resources :excursions, shallow: true do
+      resources :comments, shallow: true
+    end
   end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Comment.create( autor: 'YOBA' , body: 'YOBACOMMENT', excursion_id: 1 )
+
